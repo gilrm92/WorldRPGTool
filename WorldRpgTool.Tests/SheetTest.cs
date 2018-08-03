26410d389cfc254993119a83e8fa91bfe676e272
Gilberto Menezes <g.menezes@alfavox.pl>
2018-08-03 15:21:06 +0200

    Adding test project and starting the TDD

---
+++ WorldRpgTool.Tests/SheetTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WorldRpgTool.Tests
+{
+    [TestClass]
+    public class SheetTest
+    {
+        [TestMethod]
+        public void ShouldCreateSheet()
+        {
+            var sheetDTO = new SheetDTO()
+            {
+                Name = "Ficha modelo",
+                GameStyle = "3D&T",
+                Text = "Atributos - Forca: 0"
+            };
+
+            ISheetManager userManager = new SheetManager();
+            var sheet = userManager.CreateNewUser(sheetDTO);
+
+            Assert.AreEqual(sheetDTO.Name, sheet.Name);
+            Assert.AreEqual(sheetDTO.GameStyle, sheet.GameStyle);
+            Assert.AreEqual(sheetDTO.Text, sheet.Text);
+        }
+
+        [TestMethod]
+        public void ShouldEditSheet()
+        {
+            var sheetDTO = new SheetDTO()
+            {
+                Name = "Ficha modelo",
+                GameStyle = "3D&T",
+                Text = "Atributos - Forca: 0"
+            };
+
+            ISheetManager userManager = new SheetManager();
+            var sheet = userManager.CreateNewUser(sheetDTO);
+
+            Assert.AreEqual(sheetDTO.Name, sheet.Name);
+            Assert.AreEqual(sheetDTO.GameStyle, sheet.GameStyle);
+            Assert.AreEqual(sheetDTO.Text, sheet.Text);
+        }
+    }
+}
---
+++ WorldRpgTool.Tests/UserTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WorldRpgTool.Tests
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void ShouldCreateNewUser()
+        {
+            var userDTO = new UserDTO()
+            {
+                Name = "Gilberto",
+                Surname = "Test",
+                Email = "gilbertormm.ramos@gmail.com",
+                Password = "ASedsK23%ns@394"
+            };
+
+            IUserManager userManager = new UserManager();
+            var user = userManager.CreateNewUser(userDTO);
+
+            Assert.AreEqual(userDTO.Name, user.Name);
+            Assert.AreEqual(userDTO.Surname, user.Surname);
+            Assert.AreEqual(userDTO.Email, user.Email);
+            Assert.AreEqual(userDTO.Password, user.Password);
+        }
+
+        [TestMethod]
+        public void ShouldEditUser()
+        {
+            var userDTO = new UserDTO()
+            {
+                Name = "Gilberto",
+                Surname = "Test",
+                Email = "gilbertormm.ramos@gmail.com",
+                Password = "ASedsK23%ns@394"
+            };
+
+            IUserManager userManager = new UserManager();
+            var user = userManager.CreateNewUser(userDTO);
+
+            userDTO.Name = "Fabio";
+            userDTO.Surname = "another test";
+            userDTO.Email = "another test";
+            userDTO.Password = "another test";
+
+            var userEdited = userManager.EditUser(user, userDTO);
+
+            Assert.AreEqual(userDTO.Name, userEdited.Name);
+            Assert.AreEqual(userDTO.Surname, userEdited.Surname);
+            Assert.AreEqual(userDTO.Email, userEdited.Email);
+            Assert.AreEqual(userDTO.Password, userEdited.Password);
+        }
+    }
+}
